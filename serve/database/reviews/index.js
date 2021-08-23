2b1faf6eda17d31b0de28c1c65fb4ff3fe36fe2e
sjhalani4 <sahiljhalani4@gmail.com>
2021-08-23 20:45:24 +0530

    copy from server

---
+++ serve/database/reviews/index.js
@@ -0,0 +1,33 @@
+import  mongoose  from "mongoose";
+
+const ReviewSchema = mongoose.Schema({
+food:{
+    type: mongoose.Types.ObjectId,
+    ref: "Foods",
+},
+restaurant:
+{
+    type: mongoose.Types.ObjectId,
+    ref: "Restaurants",
+},
+User:{
+    type: mongoose.Types.ObjectId,
+    ref: "Users",
+},
+    rating: {type: Number, required: true},
+    reviewSubject:{type: String, required : true },
+    photos: [
+     {
+         type:mongoose.Types.ObjectId,
+         ref: "Images",
+     },
+    ],
+
+
+    },
+
+    {
+        timestamps: true
+    }
+    );
+    export const ReviewModel = mongoose.model("Reviews", ReviewSchema);
