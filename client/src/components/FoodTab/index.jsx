063a726b3bfc6235c80d5ef95dc0f6a7b7785186
sjhalani4 <sahiljhalani4@gmail.com>
2021-08-26 16:55:23 +0530

    add pc screen food tabs

--- client/src/components/FoodTab/index.jsx
+++ client/src/components/FoodTab/index.jsx
@@ -5,6 +5,7 @@ import {RiShoppingBag3Line} from "react-icons/ri";
 import {IoFastFoodOutline} from "react-icons/io5";
 import {BiDrink} from "react-icons/bi";
 import {IoNutritionOutline} from "react-icons/io5";
+import classnames from "classnames";
 
 
 
@@ -13,32 +14,30 @@ import {IoNutritionOutline} from "react-icons/io5";
 const MobileTabs = () =>{
     const [allTypes, setAllTypes] =useState([
         {
-        id: `delivery`,
+        id: "delivery",
         icon:<RiShoppingBag3Line/>,
         name:"Delivery",
-        isActive:false,
+       
 
         },
         {
-            id: `dining`,
+            id: "dining",
             icon:<IoFastFoodOutline/>,
             name:"Dining Out",
-            isActive:false,
+           
     
             }, 
             {
-                id: `night`,
+                id: "night",
                 icon:<BiDrink/>,
                 name:"Night life",
-                isActive:false,
-        
+               
                 },
                 {
-                    id: `nutri`,
+                    id: "nutri",
                     icon:<IoNutritionOutline/>,
                     name:"Nutrition",
-                    isActive:false,
-            
+                    
                     },
 
 
@@ -70,7 +69,7 @@ const MobileTabs = () =>{
             {
                 allTypes.map((items)=>(
                     <Link to={`${items.id}`}>
-        <div className={type===items.id ?"flex flex-col items-center text-xl text-zomato-400 relative  "  :"flex flex-col items-center text-xl"  }>
+        <div className={classnames("flex flex-col items-center text-xl" , {"flex flex-col items-center text-xl text-zomato-400 relative":type===items.id})}>
             <div className={type === items.id && "absolute -top-3 w-full border-t-2  border-zomato-400 "}/>
         {items.icon}
         <h5 className="text-sm">{items.name}</h5>
@@ -83,50 +82,73 @@ const MobileTabs = () =>{
 };
 
 const LargeTabs = () =>{
-return (<>
-<div className="hidden lg:flex gap-14 container px-20 mx-auto ">
-    <div className="flex items-center gap-3">
-        <div className="w-16 h-16 bg-gray-100 p-4 rounded-full ">
+    const [allTypes, setAllTypes] =useState([
+        {
+        id: "delivery",
+        imageDefault:"https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png",
+        imageActive:"https://b.zmtcdn.com/data/o2_assets/c0bb85d3a6347b2ec070a8db694588261616149578.png",
+        name:"Delivery",
+        activeColor:"yellow",
+       
+
+        },
+        {
+            id: "dining",
+            imageDefault:"https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png",
+            imageActive:"https://b.zmtcdn.com/data/o2_assets/30fa0a844f3ba82073e5f78c65c18b371616149662.png",
+            name:"Dining Out",
+            activeColor:"blue"
+           
+    
+            }, 
+            {
+                id: "night",
+                imageDefault:"https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png",
+                imageActive:"https://b.zmtcdn.com/data/o2_assets/855687dc64a5e06d737dae45b7f6a13b1616149818.png",
+                name:"Night life",
+                activeColor:"purple"
+               
+                },
+                {
+                    id: "nutri",
+                    imageDefault:"https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png",
+                    imageActive:"https://b.zmtcdn.com/data/o2_assets/0f6dcb1aef93fa03ea3f91f37918f3bc1616649503.png",
+                     name:"Nutrition",
+                     activeColor:"yellow",
+                    
+                    },
+
+
+
+    ]);
+    const {type} = useParams();
+    return (<>
+        <div className="hidden lg:flex gap-14 container px-20 mx-auto ">
             
-            <img src="https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png" alt="Delivery"
-            className="w-full h-full  " />
-        </div>
-        <h3 className="text-xl text-gray-700 ">Delivery</h3>
-
-    </div>
-
-    <div className="flex items-center gap-3">
-        <div className="w-16 h-16 bg-gray-100 p-4 rounded-full ">
+        
+            {allTypes.map((items)=> (
+                <Link to={`/${items.id}`}>
+                    <div className={classnames("flex items-center gap-3",{"border-b-2 border-zomato-400 pb-2 transition duration-1000 ease-in-out":type===items.id})}>
+                <div className={classnames("w-16 h-16 p-4 rounded-full bg-gray-100",{[`bg-${items.activeColor}-100`]:type===items.id}) }>
+                    
+                    <img src={type===items.id?items.imageActive:items.imageDefault} alt="Delivery"
+                    className="w-full h-full  " />
+                </div>
+                <h3 className={type===items.id?"text-xl text-zomato-400":"text-xl text-gray-700"}>{items.name}</h3>
+        
+                </div>
+            </Link>
+              ) )}
+            </div>
+        
+            </>
+            );
+            };
+        
+    
             
-            <img src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png" alt="Dining Out"
-            className="w-full h-full  " />
-        </div>
-        <h3 className="text-xl text-gray-700 ">Dining Out</h3>
-
-    </div>
-
-    <div className="flex items-center gap-3">
-        <div className="w-16 h-16 bg-gray-100 p-4 rounded-full ">
-            
-            <img src="https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png" alt="NightLife"
-            className="w-full h-full  " />
-        </div>
-        <h3 className="text-xl text-gray-700 ">Nightlife</h3>
         
 
-    </div>
-
-    <div className="flex items-center gap-3">
-        <div className="w-16 h-16 bg-gray-100 p-4 rounded-full ">
-            
-            <img src="https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png" alt="Night"
-            className="w-full h-full  " />
-        </div>
-        <h3 className="text-xl text-gray-700 ">Nutrition</h3>
-        
-
-    </div>
-
     
 
     
@@ -135,11 +157,13 @@ return (<>
 
 
 
-</div>
+    
 
-</>
-);
-};
+    
+
+
+
+
 
 const FoodTab = () => {
     return (<>
